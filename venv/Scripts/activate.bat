357f3bd0f813c702ad9abce6dd6355771eb6f771
ThOMaZMe11o <162379221+ThOMaZMe11o@users.noreply.github.com>
2024-11-06 09:22:17 -0300

    small adjustments

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\katel\OneDrive\Documentos\GitHub\flask-com-mysql\venv
+set VIRTUAL_ENV=C:\Users\Manhã\Documents\GitHub\API-1\flask-com-mysql\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
